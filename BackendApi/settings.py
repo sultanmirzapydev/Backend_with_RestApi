aac8040e52889b4be4995fcddccfbe7436805918
sultanmirzapydev <sultanmirzapydev@gmail.com>
2021-06-18 02:01:09 +0530

    *

--- BackendApi/settings.py
+++ BackendApi/settings.py
@@ -131,13 +131,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- BackendApi/urls.py
+++ BackendApi/urls.py
@@ -38,5 +38,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
-    #path('api/getpro/' , PhotographerPic.as_view(), name='get_pro') 
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/getpro/' , PhotographerPic.as_view(), name='get_pro') 
+    ]
+
+
+     # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

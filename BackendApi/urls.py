a0ca8b7abd42114c2cd7f5f87fefa4771fc61fd4
sultanmirzapydev <sultanmirzapydev@gmail.com>
2021-06-18 02:13:04 +0530

    *

--- BackendApi/urls.py
+++ BackendApi/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 from users.views import UserViewSet
 from images.views import ImageListAPIView, ImageSearch, ImgGet
@@ -42,4 +42,4 @@ urlpatterns = [
     ]
 
 
-     # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

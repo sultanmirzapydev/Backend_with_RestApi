b3b8d1d38d71dff793e93ae2deea8bc340a48cc8
sultanmirzapydev <sultanmirzapydev@gmail.com>
2021-06-17 19:21:27 +0530

    *

--- BackendApi/settings.py
+++ BackendApi/settings.py
@@ -87,13 +87,8 @@ WSGI_APPLICATION = 'BackendApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'api',
-        'USER' : 'postgres',
-        'PASSWORD' : '6976',
-        'HOST ': 'localhost',
-        'PORT': '5432'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

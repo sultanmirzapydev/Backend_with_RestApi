f6c5a6285898f6047e928e8a0588f1a06fb715d4
sultanmirzapydev <sultanmirzapydev@gmail.com>
2021-06-18 02:09:15 +0530

    m

--- BackendApi/urls.py
+++ BackendApi/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
-    path('api/getpro/' , PhotographerPic.as_view(), name='get_pro') 
+    # path('api/getpro/' , PhotographerPic.as_view(), name='get_pro') 
     ]
 
 

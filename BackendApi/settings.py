14ca3d1a394340324b59b5a8aa0b2db077b1e7a8
sultanmirzapydev <sultanmirzapydev@gmail.com>
2021-06-18 11:43:47 +0530

    *

--- BackendApi/settings.py
+++ BackendApi/settings.py
@@ -87,11 +87,15 @@ WSGI_APPLICATION = 'BackendApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reactsuppdb',
+        'USER': 'root',
+        'PASSWORD': '6976',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
+# mysql -uroot -p
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
